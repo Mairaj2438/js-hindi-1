e7e01ce831340b2b0199e2319fcf3f2617b5456e
Mairaj2438 <mk3146570@gmail.com>
2025-09-15 23:57:10 -0700

    practice changes

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 console.log('haider')
+console.log('let code developer')
+console.log('programmer')
+console.log('dream to be a mern stack developer')
+
+
+const FruitOne = 37467
+let RedFruit = "Mango"
+let RedTwoFruit = "grapes"
+let RedThreeFruit = "pomigranate"
+let blackFruuit = "dates"
+
+console.table([FruitOne,RedFruit,RedTwoFruit,RedTwoFruit,RedThreeFruit,blackFruuit])
